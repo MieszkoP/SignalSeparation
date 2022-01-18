62213ea16a4699e0f1dec57485ca94407e119272
MieszkoP <78937784+MieszkoP@users.noreply.github.com>
2022-01-18 13:46:04 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='signal_separation',
     packages=['signal_separation'],
-    version='0.0.7',
+    version='0.0.8',
     license='MIT',
     description='Algorithm for generating and separating analytical chemistry signal.',
     author='Mieszko Pasierbek',
     author_email='mieszko.pasierbek@gmail.com',
     url='https://github.com/MieszkoP',
-    download_url='https://github.com/MieszkoP/signal_separation/archive/refs/tags/0.0.7.tar.gz',
+    download_url='https://github.com/MieszkoP/signal_separation/archive/refs/tags/0.0.8.tar.gz',
     keywords=['CNN', 'Deep Learning', 'signal', 'SVR', 'chemometrics', 'chemistry'],
     install_requires=[
         'numpy',
--- signal_separation/_file_downloader.py
+++ signal_separation/_file_downloader.py
@@ -1,14 +1,19 @@
 import pickle
-
-
+from pathlib import Path
 def download_svrt():
-    return pickle.load(open('signal_separation/trained/SVRp', 'rb'))
+    p = Path(__file__).parent
+    p = Path(p, 'trained', 'SVRp')
+    return pickle.load(open(p, 'rb'))
 
 
 def download_svrh():
-    return pickle.load(open('signal_separation/trained/SVRh', 'rb'))
+    p = Path(__file__).parent
+    p = Path(p, 'trained', 'SVRh')
+    return pickle.load(open(p, 'rb'))
 
 
 def download_weights(model):
-    model.load_weights('signal_separation/trained/neural_network.h5')
+    p = Path(__file__).parent
+    p = Path(p, 'trained', 'neural_network.h5')
+    model.load_weights(p)
     pass
