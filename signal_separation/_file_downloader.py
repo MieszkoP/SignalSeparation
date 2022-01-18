b24b6f20543f34d2fc84388e63872e2bd8946941
MieszkoP <78937784+MieszkoP@users.noreply.github.com>
2022-01-18 13:55:43 +0100

    Add files via upload

--- signal_separation/_file_downloader.py
+++ signal_separation/_file_downloader.py
@@ -2,18 +2,18 @@ import pickle
 from pathlib import Path
 def download_svrt():
     p = Path(__file__).parent
-    p = Path(p, 'trained', 'SVRp')
+    p = Path(p, 'SVRp')
     return pickle.load(open(p, 'rb'))
 
 
 def download_svrh():
     p = Path(__file__).parent
-    p = Path(p, 'trained', 'SVRh')
+    p = Path(p, 'SVRh')
     return pickle.load(open(p, 'rb'))
 
 
 def download_weights(model):
     p = Path(__file__).parent
-    p = Path(p, 'trained', 'neural_network.h5')
+    p = Path(p, 'neural_network.h5')
     model.load_weights(p)
     pass
