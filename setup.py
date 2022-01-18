3b2e8376a6cb7e90df9d6b702c17189a19dce178
MieszkoP <78937784+MieszkoP@users.noreply.github.com>
2022-01-18 20:10:17 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from distutils.core import setup
 setup(
     name='signal_separation',
     packages=['signal_separation'],
-    version='0.0.9',
+    version='0.0.10',
     license='MIT',
     description='Algorithm for generating and separating analytical chemistry signal.',
     author='Mieszko Pasierbek',
     author_email='mieszko.pasierbek@gmail.com',
     url='https://github.com/MieszkoP',
-    download_url='https://github.com/MieszkoP/signal_separation/archive/refs/tags/0.0.9.tar.gz',
+    download_url='https://github.com/MieszkoP/signal_separation/archive/refs/tags/0.0.10.tar.gz',
     keywords=['CNN', 'Deep Learning', 'signal', 'SVR', 'chemometrics', 'chemistry'],
+    include_package_data=True,
     install_requires=[
         'numpy',
         'tensorflow'
